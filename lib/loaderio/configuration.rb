3857e8460df280b109a7492c5674a01e61df6b67
Ryan Her <ryan@quikly.com>
2014-10-23 11:27:26 -0400

    Use SSLv23 instead of vulnerable SSLv3

--- lib/loaderio/configuration.rb
+++ lib/loaderio/configuration.rb
@@ -16,7 +16,7 @@ module Loaderio
 
     def resource
       raise "LOADERIO_API_KEY needs to be set" unless api_key
-      RestClient::Resource.new(base_url, headers: {"loaderio-Auth" => api_key, content_type: :json, accept: :json })
+      RestClient::Resource.new(base_url, headers: {"loaderio-Auth" => api_key, content_type: :json, accept: :json }, ssl_version: 'SSLv23')
     end
   end
 end
