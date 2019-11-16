8b38df5ec17dfaaf3480540e758f3485fc69b2b7
Rich Humphrey <rich.humphrey@masterclass.com>
2019-11-16 14:46:20 -0800

    Updated to v2 API. Added Test.run

--- lib/loaderio/test.rb
+++ lib/loaderio/test.rb
@@ -1,7 +1,7 @@
 module Loaderio
   class Test < Base
     attr_reader :url, :name, :duration, :timeout, :from, :to, :status, :test_id, :request_type, :results_data
-    
+
     def initialize(attributes)
       @url          = attributes[:url]
       @name         = attributes[:name]
@@ -15,15 +15,19 @@ module Loaderio
       @results_data = OpenStruct.new(attributes[:results_data])
       super
     end
-    
+
     def self.resource_name
       "tests"
     end
-    
+
     def self.results(id)
       new(parse(Loaderio::Configuration.resource["#{resource_name}/#{id}/results"].get))
     end
-    
+
+    def self.run(id)
+      new(parse(Loaderio::Configuration.resource["#{resource_name}/#{id}/run"].put({})))
+    end
+
     def self.stop(id)
       new(parse(Loaderio::Configuration.resource["#{resource_name}/#{id}/stop"].put({})))
     end
